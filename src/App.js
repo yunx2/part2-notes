5442e7906af7d4fe44d3fb25127ad31d356a4c8b
Matti Luukkainen <mluukkai@iki.fi>
2020-02-01 17:39:21 +0200

    part5-3

--- src/App.js
+++ src/App.js
@@ -22,6 +22,15 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -65,6 +74,11 @@ const App = () => {
         username, password,
       })
 
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
