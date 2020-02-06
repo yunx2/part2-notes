698911fe5ba14fac56324c36219fd46e122c28e4
Matti Luukkainen <mluukkai@iki.fi>
2020-02-06 21:59:14 +0200

    part5-11

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -52,19 +52,14 @@ describe('Note app', function() {
 
     describe('and a note exists', function () {
       beforeEach(function () {
-        cy.createNote({
-          content: 'another note cypress',
-          important: false
-        })
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
       })
 
       it('it can be made important', function () {
-        cy.contains('another note cypress')
-          .contains('make important')
-          .click()
-
-        cy.contains('another note cypress')
-          .contains('make not important')
+        cy.contains('second note').parent().find('button').click()
+        cy.contains('second note').parent().find('button').should('contain', 'make not important')
       })
     })
   })
