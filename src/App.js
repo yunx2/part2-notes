36a6f8758a120841875ed8ef9a3f0f072fbb7004
Yunyun <celeph4is@gmail.com>
2021-02-09 11:09:16 -0800

    end of 2c

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+  
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, []) // empty array as dependencies parameter means only run effect after the first render of the componentl; makes sense since if the effect has no dependencies, then nothing will trigger its running again
+  console.log('render', notes.length, 'notes')
+
     
   const handleNoteChange = (event) => {
     console.log(event.target.value)
